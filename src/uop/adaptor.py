c2294e4dde25172d02d22a3377e78c2c5c652bba
Samantha Atkins <samantha@sjasoft.com>
2025-11-11 14:06:06 -0500

    initial

---
+++ src/uop/adaptor.py
@@ -0,0 +1,8 @@
+from uop.db.alchemy.adaptor import AlchemyDatabase, AlchemyCollection
+
+
+class SQLiteDatabase(AlchemyDatabase):
+    def __init__(self, dbname, tenant_id=None, *schemas, **db_credentials):
+        super().__init__(
+            dbname, tenant_id=tenant_id, db_brand="sqlite", *schemas, **db_credentials
+        )
